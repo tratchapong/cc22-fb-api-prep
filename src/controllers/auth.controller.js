b1ed60eede2687204cd4944b91540556c1548efb
Ratchapong <ratchapong@Ratchapongs-Mac-mini.local>
2026-03-09 00:23:36 +0700

    Login with validation

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,37 +1,66 @@
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 import createHttpError from 'http-errors'
 import { prisma } from '../lib/prisma.js'
 import identityKeyUtil from '../utils/identity-key.util.js'
-import { registerSchema } from '../validations/schema.js'
+import { loginSchema, registerSchema } from '../validations/schema.js'
 
 
 export async function register(req, res, next) {
 
- // validation
- const data = await registerSchema.parseAsync(req.body)
- const identityKey = data.email ? 'email' : 'mobile'
+  // validation
+  const data = await registerSchema.parseAsync(req.body)
+  const identityKey = data.email ? 'email' : 'mobile'
 
- // find user for non-duplicate
- const haveUser = await prisma.user.findUnique({
-   where : { [identityKey] : data[identityKey] }})
- if(haveUser) {
-   return next(createHttpError[409]('This user already register')) }
+  // find user for non-duplicate
+  const haveUser = await prisma.user.findUnique({
+    where: { [identityKey]: data[identityKey] }
+  })
+  if (haveUser) {
+    return next(createHttpError[409]('This user already register'))
+  }
 
-// create new user in DB
- const result = await prisma.user.create({data : data})
- res.json({
-   msg : 'Register Successful',
-   result : result
-  }) 
+  // create new user in DB
+  const result = await prisma.user.create({ data: data })
+  res.json({
+    message: 'Register Successful',
+    result: result
+  })
 }
 
-
 export async function login(req, res, next) {
+  // validation
+  const data = loginSchema.parse(req.body)
+  const identityKey = data.email ? 'email' : 'mobile'
 
-  res.json({message: 'Login service'})
+  // find user in DB
+  const foundUser = await prisma.user.findFirst({
+    where: { [identityKey]: data[identityKey] }
+  })
+  if (!foundUser) {
+    return next(createHttpError[409]('Invalid login 1'))
+  }
+
+  // check password
+  let pwOk = await bcrypt.compare(data.password, foundUser.password)
+  if (!pwOk) { return next(createHttpError[401]('Invalid Login 2')) }
+
+  //  create token
+  const payload = { id: foundUser.id }
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    algorithm: 'HS256',
+    expiresIn: '15d'
+  })
+  //  rip password, createdAt, updatedAt
+  const { password: pw, createdAt, updatedAt, ...userData } = foundUser
+  res.json({
+    msg: 'Login Successful',
+    token: token,
+    user: userData
+  })
 }
 
 export async function getMe(req, res, next) {
-  
-  res.json({message: 'Getme service'})
+
+  res.json({ message: 'Getme service' })
 }
--- src/validations/schema.js
+++ src/validations/schema.js
@@ -7,24 +7,35 @@ const mobileRegex = /^[0-9]{10,15}$/
 const identityKey = val => emailRegex.test(val) ? 'email' : mobileRegex.test(val) ? 'mobile' : ''
 
 export const registerSchema = z.object({
-   identity : z.string().min(2, "Email or phone-number require")
-   .refine(value => emailRegex.test(value) || mobileRegex.test(value), {
-       message : "identity must be a valid email or mobile number"
-   }),
-   firstName : z.string().min(2, "first name is required"),
-   lastName : z.string().min(2, "last name is required"),
-   password : z.string().min(4, "password at least 4 characters"),
-   confirmPassword : z.string().min(4, "confirm password is required"),
-}).refine( data => data.password === data.confirmPassword , {
-   message : 'confirmPassword must match password',
+   identity: z.string().min(2, "Email or phone-number require")
+      .refine(value => emailRegex.test(value) || mobileRegex.test(value), {
+         message: "identity must be a valid email or mobile number"
+      }),
+   firstName: z.string().min(2, "first name is required"),
+   lastName: z.string().min(2, "last name is required"),
+   password: z.string().min(4, "password at least 4 characters"),
+   confirmPassword: z.string().min(4, "confirm password is required"),
+}).refine(data => data.password === data.confirmPassword, {
+   message: 'confirmPassword must match password',
    path: ['confirmPassword']
-}).transform( async data => {
+}).transform(async data => {
    const output = {
-    [identityKey(data.identity)] : data.identity,
-    firstName : data.firstName,
-    lastName : data.lastName,
-    password : await bcrypt.hash(data.password, 8)
+      [identityKey(data.identity)]: data.identity,
+      firstName: data.firstName,
+      lastName: data.lastName,
+      password: await bcrypt.hash(data.password, 8)
    }
    return output
 })
 
+export const loginSchema = z.object({
+   identity: z.string().min(2, "Email or phone-number require")
+      .refine(value => emailRegex.test(value) || mobileRegex.test(value), {
+         message: "identity must be a valid email or mobile number"
+      }),
+   password: z.string().min(4, "password at least 4 characters")
+}).transform(data => ({
+      [identityKey(data.identity)]: data.identity,
+      password: data.password
+   })
+)
